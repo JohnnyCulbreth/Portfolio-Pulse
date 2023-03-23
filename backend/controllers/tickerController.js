5cce428c940948726f8a78cc161bf7e468265356
JohnnyCulbreth <johnnyculbrethjr@yahoo.com>
2023-03-23 09:11:02 -0500

    fixed addStock bug

--- backend/controllers/tickerController.js
+++ backend/controllers/tickerController.js
@@ -6,7 +6,7 @@ const axios = require('axios');
 // FETCH Stock info
 const fetchStockInfo = async (stockSymbol) => {
   // const API_KEY = process.env.IEX_API_KEY;
-  const API_KEY = 'pk_b8445edb92244ad88a3de425568b1d07';
+  const API_KEY = 'pk_c227bbfffc334f619036e0e3d8679fb5';
   const url = `https://cloud.iexapis.com/stable/stock/${stockSymbol}/quote?token=${API_KEY}`;
   const response = await axios.get(url);
   return response.data;
